d6265c584a844227db9b19297f980e57ec2b3e25
Thummala Jantima <thummalajantima@thummalas-air.home>
2022-02-25 15:17:33 +0100

    update

--- App.js
+++ App.js
@@ -3,13 +3,11 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { Hello } from './src/screens/SplashScreen';
 import { WarrantyDe } from './src/screens/WarrantyDetail';
 import {WarrantyList} from './src/screens/Dashboard';
-import {ProgressIndicator} from './src/screens/ActivityIndicator'
 
 const AppNavigator = createStackNavigator({
-  // 'Warranty Detail' : WarrantyDe 
+  'Warranty Detail' : WarrantyDe 
   // Warranties: WarrantyList
   // 'Warranty Reminder' : Hello
-  'Progress' : ProgressIndicator
 });
 
 export default createAppContainer(AppNavigator);
--- src/screens/WarrantyDetail.js
+++ src/screens/WarrantyDetail.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Image, Text, View, StyleSheet } from 'react-native';
+import { Image, Text, View, StyleSheet, ActivityIndicator } from 'react-native';
 import {NewWarrantyForm} from '../components/NewWarrantyForm';
 import {AddImageButton} from '../components/Button';
-import {UploadButton} from '../components/UploadButton'
 import {AddWarrantyButton} from '../components/Button';
 
 const WarrantyDe = ({ navigation }) => {
     return (
         <View style={{flex:1, justifyContent: 'center', alignContent: 'center'}}>
             <NewWarrantyForm />
-            {/* <UploadButton /> */}
-            {/* <AddImageButton /> */}
+            <ActivityIndicator size='large' color='gray'/>
             <AddWarrantyButton />
 
         </View>
